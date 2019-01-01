ffcb390be602a5d744a137ba40fe7716ed41c156
Till <vm06381592966573992v@vmail.inf.h-brs.de>
2019-01-01 20:18:30 +0100

    Tabs → Spaces

--- src/SpielerVerhaltenBenutzer.java
+++ src/SpielerVerhaltenBenutzer.java
@@ -5,30 +5,30 @@ public class SpielerVerhaltenBenutzer extends SpielerVerhalten {
     public SpielerVerhaltenBenutzer(KartenStapel ziehstapel, KartenStapel ablagestapel,
         ArrayList<KartenListe> spielfeld, KartenListe hand)
     {
-    	super(ziehstapel, ablagestapel, spielfeld, hand);
+        super(ziehstapel, ablagestapel, spielfeld, hand);
     }
 
-	public void ziehen() {
+    public void ziehen() {
 
-	}
-	
-	public void spielzugPruefen() {
+    }
+    
+    public void spielzugPruefen() {
 
-	}
+    }
 
-	public void auslegen() {
-		// DEBUG: In 50% der Fälle eine Karte ablegen
-	    if (new java.util.Random().nextInt(2) == 0) {
+    public void auslegen() {
+        // DEBUG: In 50% der Fälle eine Karte ablegen
+        if (new java.util.Random().nextInt(2) == 0) {
             hand.remove(0);
         }
-	}
+    }
 
-	public void anlegen() {
+    public void anlegen() {
 
-	}
+    }
 
-	public void ablegen() {
+    public void ablegen() {
 
-	}
+    }
 
 }
